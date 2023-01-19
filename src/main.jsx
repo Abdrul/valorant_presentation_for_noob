6d9312eedad231c5dd0fb38768e4538d2662b53d
Abel <josephine.abel@outlook.fr>
2023-01-19 09:21:23 +0100

    try to animate svg

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,5 @@
 import ReactDOM from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import React from "react";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
@@ -11,7 +10,6 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
       <QueryClientProvider client={queryClient}>
-        <ReactQueryDevtools initialIsOpen={true} />
         <App />
       </QueryClientProvider>
     </BrowserRouter>
